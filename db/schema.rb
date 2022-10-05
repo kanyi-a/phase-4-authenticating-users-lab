d42f27acf773dc79e0c34cae1935dd31679ad97a
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-05 15:17:53 +0300

    commit tree 42add7999057fd02e9f84e0cd3c7ab1f698a6ff1

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_092801) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.integer "minutes_to_read"
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "articles", "users"
 end
